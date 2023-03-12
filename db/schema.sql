981e74aff926c46fef184a2fd5d8787df3c95bcd
Xhway <dhway20@gmail.com>
2023-03-11 21:50:53 -0500

    updated schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employees_db;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30)
     
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE employees(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY(role_id) REFERENCES roles(id),
     FOREIGN KEY(manager_id) REFERENCES employees(id)
     ON DELETE SET NULL
